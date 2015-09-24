d6eb5dc67677122576beaf85e37a6da4a01ab43e
houshipeng <hsp2004@126.com>
2015-09-24 18:00:16 +0800

    增加支付

---
+++ ksb-api/src/main/java/com/ksb/openapi/mobile/service/ChargeService.java
@@ -0,0 +1,16 @@
+package com.ksb.openapi.mobile.service;
+
+public interface ChargeService {
+
+	/**
+	 * 商家充值
+	 * @param channelName 付费渠道
+	 *        (alipay:支付宝APP支付 ; alipay_qr:支付宝扫码支付 ; wx:微信支付 ; wx_pub:微信公众号支付 ; wx_pub_qr:微信公众号扫码 ; bfb:百度钱包支付)
+	 * @param amount      充值金额
+	 * @param spId        商家编号
+	 * @param pingxxAppId appid
+	 */
+	public void shipperRecharge(String channelName,String amount,String spId,String pingxxAppId);
+	
+	
+}
